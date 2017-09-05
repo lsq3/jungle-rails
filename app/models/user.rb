55e0389e746a3a582927380225d1967fc3d6eb0b
Sean <leoseanquilty@gmail.com>
2017-09-05 20:50:12 +0000

    Remove erroneous file & implement user test

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
 
     has_many :reviews
 
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true
+    validates :password, presence: true
+    validates :password_confirmation, presence: true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Users, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model do
+
+  describe 'Validations' do
+    
+    it "Should validate a password is supplied" do
+      @u = User.create(first_name: "dude", last_name: "duderino", email: "dude@thedude.com", password: nil, password_confirmation: "pass")
+      @u.save
+      expect(@u).to be_invalid
+    end
+
+
+
+
+  end
+
 end
